c5b59d838b5a733499d81a414907baa7239f1a5c
MarkMizzi <mizzimark2001@gmail.com>
2022-12-29 11:29:54 +0100

    first working version of planner

--- souffle_tools/visitors/ram_plan.py
+++ souffle_tools/visitors/ram_plan.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Dict, List, Optional
 
+import lark.tree
+import lark.lexer
 from lark.visitors import Interpreter
 
 from .utils import has_child
@@ -23,7 +25,7 @@ class PlanNode:
             + self.seperator.join(
                 map(lambda x: x.__str__(prefix=prefix + "   "), self.children)
             )
-            + (f"\n{self.trailer}" if self.trailer is not None else ""),
+            + (f"\n{self.trailer}" if self.trailer is not None else "")
         )
 
 
@@ -31,12 +33,24 @@ class TextualPlanVisitor(Interpreter):
     def __init__(self, rels: Dict[str, Relation]):
         self.out = ""
         self._plan = None  # for gradully building plans BOTTOM UP.
+        self.rels = rels
+        self._bound_tuples = {}
+
+    def _safe_visit(self, tree: lark.tree.Tree | lark.lexer.Token):
+        if isinstance(tree, lark.tree.Tree):
+            return self.visit(tree)
+        else:
+            return str(tree)
 
     def program(self, tree) -> PlanNode:
         return PlanNode("PROGRAM", list(map(self.visit, tree.children[1:-1])))
 
     def subroutine(self, tree) -> PlanNode:
-        return PlanNode(str(tree.children[0]), list(map(self.visit, tree.children[1:])))
+        return PlanNode(
+            str(tree.children[0]),
+            list(map(self.visit, tree.children[1:])),
+            seperator=";\n",
+        )
 
     ### Subroutine statements
 
@@ -76,12 +90,12 @@ class TextualPlanVisitor(Interpreter):
             raise RuntimeError(f"Unrecognized IO operation {io_options['operation']}")
 
         fname = ""
-        if io_options["filename"].removesuffix(".dl") != relname:
-            fname = f"fname {io_options['filename']}"
+        if io_options.get("filename", relname).removesuffix(".dl") != relname:
+            fname = f" fname {io_options['filename']}"
 
-        return PlanNode(
-            f"{operation} {relname} delim {repr(io_options['delimiter'])} {fname}", []
-        )
+        delim = repr(io_options.get("delimiter", "\t"))
+
+        return PlanNode(f"{operation} {relname} delim {delim}{fname}", [])
 
     ### RAM statements
 
@@ -89,7 +103,7 @@ class TextualPlanVisitor(Interpreter):
         assert self._plan is not None
 
         tuple_element_ref = self.visit(tree.children[0])
-        aggregator = self.visit(tree.children[1])
+        aggregator = str(tree.children[1])
         tuple_name = str(tree.children[2])
         ram_relname = self.visit(tree.children[3])
 
@@ -108,6 +122,11 @@ class TextualPlanVisitor(Interpreter):
         tuple_name = str(tree.children[0])
         ram_relname = self.visit(tree.children[1])
 
+        relname = self.visit(tree.children[1].children[-1])
+
+        # NOTE: again this is something where we might want to use context managers.
+        self._bound_tuples[tuple_name] = self.rels[relname]
+
         # add index condition, if there is one.
         cond = ""
         if has_child(tree, "on_index"):
@@ -117,7 +136,11 @@ class TextualPlanVisitor(Interpreter):
             PlanNode(f"{tuple_name} ∈ {ram_relname}, {cond}", [])
         )
 
-        return self.visit(tree.children[-1])
+        plan = self.visit(tree.children[-1])
+
+        # we're exiting the scope where this tuple_name is valid.
+        del self._bound_tuples[tuple_name]
+        return plan
 
     def if_stmt(self, tree):
         assert self._plan is not None
@@ -129,7 +152,7 @@ class TextualPlanVisitor(Interpreter):
             # add index condition, if there is one.
             index_cond = ""
             if has_child(tree, "on_index"):
-                index_cond = f" USING INDEX {self.visit(tree.children[2])}"
+                index_cond = f" USING INDEX {self.visit(tree.children[1])}"
 
             self._plan.children.append(PlanNode(f"{if_cond}{index_cond}", []))
 
@@ -139,10 +162,10 @@ class TextualPlanVisitor(Interpreter):
         assert self._plan is not None
 
         # TODO: Ideally we resolve ADT types here.
-        iden = self.visit(tree.children[0])
+        iden = str(tree.children[0])
         tuple_element_ref = self.visit(tree.children[2])
 
-        self._plan.children.append(PlanNode(f"{iden} = {tuple_element_ref}", []))
+        self._plan.children.append(PlanNode(f"{iden} := {tuple_element_ref}", []))
 
         return self.visit(tree.children[-1])
 
@@ -166,6 +189,104 @@ class TextualPlanVisitor(Interpreter):
         return self.visit(tree.children[0])
 
     ### RAM conditions
+
+    # TODO: Everything from ram_cond down. Don't forget to use self.rels for resolving tuple elements.
+    def ram_cond(self, tree):
+        def is_complex(tree: lark.tree.Tree):
+            return len(tree.children) > 0
+
+        def bracket_complex(tree: lark.tree.Tree | lark.lexer.Token):
+            if isinstance(tree, lark.tree.Tree):
+                if is_complex(tree):
+                    return f"({self.visit(tree)})"
+                else:
+                    return self.visit(tree)
+            else:
+                return str(tree)
+
+        return ", ".join(map(bracket_complex, tree.children))
+
+    def or_cond(self, tree):
+        return "; ".join(self.visit_children(tree))
+
+    def and_cond(self, tree):
+        return ", ".join(self.visit_children(tree))
+
+    def not_cond(self, tree):
+        return "!" + self.visit(tree.children[0])
+
+    def in_cond(self, tree):
+        return " ∈ ".join(self.visit_children(tree))
+
+    def exists_cond(self, tree):
+        return "∃" + str(tree.children[0]) + " ∈ " + self.visit(tree.children[1])
+
+    def isempty_cond(self, tree):
+        return self.visit(tree.children[0]) + " = ∅"
+
+    def comparision(self, tree):
+        return (
+            self.visit(tree.children[0])
+            + f" {str(tree.children[1])} "
+            + self.visit(tree.children[2])
+        )
+
+    def bracketed_cond(self, tree):
+        return f"({self.visit(tree.children[0])})"
+
+    def tuple_expr(self, tree):
+        return f"〈{','.join(self.visit_children(tree))}〉"
+
+    def add_tuple_element(self, tree):
+        return " + ".join(self.visit_children(tree))
+
+    def sub_tuple_element(self, tree):
+        return " - ".join(self.visit_children(tree))
+
+    def undef(self, _):
+        return "UNDEFINED"
+
+    def tuple_element_literal(self, tree):
+        return str(tree.children[0])
+
+    def tuple_element_ref(self, tree):
+        tuple_name = str(tree.children[0])
+
+        colname = str(tree.children[1])
+        if tuple_name in self._bound_tuples:
+            # NOTE: We need this check because tuple_element_ref can also be used for indexing into types.
+            colname = self._bound_tuples[tuple_name].attrs[int(colname)][0]
+
+        return ".".join([tuple_name, colname])
+
+    def functor_call(self, tree):
+        return (
+            self._safe_visit(tree.children[0])
+            + "("
+            + ",".join(map(self.visit, tree.children[1:]))
+            + ")"
+        )
+
+    def userdef_functor(self, tree):
+        return "@" + self.visit(tree.children[0])
+
+    def bracketed_tuple_element(self, tree):
+        return f"({self.visit(tree.children[0])})"
+
+    def record_tuple_element(self, tree):
+        return "[" + ",".join(self.visit_children(tree)) + "]"
+
+    def ram_relname(self, tree):
+        relname = self.visit(tree.children[-1])
+        prefix = ""
+        if len(tree.children) > 1:
+            relkind = str(tree.children[0])
+
+            if relkind == "@new_":
+                prefix = "Δ[i+1]"
+            elif relkind == "@delta_":
+                prefix = "Δ[i]"
+        return f"{prefix}{relname}"
 
     ### Building blocks
 
