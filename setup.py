4c4c70320d78334ee4568f96ff781671b0ecc650
MarkMizzi <mizzimark2001@gmail.com>
2022-12-29 19:54:14 +0100

    added grammars to setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,9 @@ setup(
     version="1.0.0",
     packages=find_packages(),
     install_requires=["lark", "typer"],
+    package_data={
+        "souffle_tools": [
+            "grammars/*.lark",
+        ],
+    },
 )
